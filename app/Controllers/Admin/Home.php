63606b513b4c3d987fb685e7bca5d8e71687d976
shallombiyan <terminalkuning3@gmail.com>
2024-11-29 14:32:49 +0700

    Perbaiki Tampilan Dashboard Admin

--- app/Controllers/Admin/Home.php
+++ app/Controllers/Admin/Home.php
@@ -3,14 +3,28 @@
 namespace App\Controllers\Admin;
 
 use App\Controllers\BaseController;
+use App\Models\PegawaiModel;
+use App\Models\PresensiModel;
+use App\Models\KetidakhadiranModel;
 use CodeIgniter\HTTP\ResponseInterface;
 
 class Home extends BaseController
 {
     public function index()
     {
+        date_default_timezone_set('Asia/Jakarta');
+        $pegawai_model = new PegawaiModel();
+        $presensi_model = new PresensiModel();
+        $ketidakhadiran_model = new KetidakhadiranModel();
+
+
         $data = [
-            'title' => 'Home'
+            'title' => 'Home',
+            'total_pegawai' => $pegawai_model->countAllResults(),
+            'total_hadir' => $presensi_model->where('tanggal_masuk', date('Y-m-d'))->countAllResults(),
+            'ketidakhadiran' => $ketidakhadiran_model->where('tanggal', date('Y-m-d'))->countAllResults()
+
+
         ];
         return view('admin/home', $data);
     }
--- app/Views/admin/home.php
+++ app/Views/admin/home.php
@@ -2,6 +2,9 @@
 
 <?= $this->section('content') ?>
 
+<?php  date_default_timezone_set('Asia/Jakarta'); ?>
+<span class="mb-3">Data tanggal <?= date('d F Y') ?></span>
+
 <div class="row">
   <div class="col-xl-3 col-lg-4 col-sm-6">
     <div class="icon-card mb-30">
@@ -10,7 +13,7 @@
       </div>
       <div class="content">
         <h6 class="mb-10">Total Pegawai</h6>
-        <h3 class="text-bold mb-10">0</h3>
+        <h3 class="text-bold mb-10"><?= $total_pegawai ?></h3>
       </div>
     </div>
     <!-- End Icon Card -->
@@ -23,7 +26,7 @@
       </div>
       <div class="content">
         <h6 class="mb-10">Hadir</h6>
-        <h3 class="text-bold mb-10">0</h3>
+        <h3 class="text-bold mb-10"><?= $total_hadir ?></h3>
       </div>
     </div>
     <!-- End Icon Card -->
@@ -36,7 +39,7 @@
       </div>
       <div class="content">
         <h6 class="mb-10">Alpha</h6>
-        <h3 class="text-bold mb-10">0</h3>
+        <h3 class="text-bold mb-10"><?= $total_pegawai - $ketidakhadiran ?></h3>
       </div>
     </div>
     <!-- End Icon Card -->
@@ -49,7 +52,7 @@
       </div>
       <div class="content">
         <h6 class="mb-10">Cuti/Izin/Sakit</h6>
-        <h3 class="text-bold mb-10">0</h3>
+        <h3 class="text-bold mb-10"><?= $ketidakhadiran ?></h3>
       </div>
     </div>
     <!-- End Icon Card -->
